29002c39c6c9bb83bf31cb487d4e415517806055
dileepreddy007 <dileepreddy.dr24@gmail.com>
2022-03-11 23:03:33 +0530

    hof

--- hof/src/App.js
+++ hof/src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import FilterByAge from './components/filterbyage';
+import FilterByLetter from './components/filterbystartletter';
+import HOF from './components/HigherOrderComponent';
+import TotalExperience from './components/totalexperience';
+import UserType from './components/UserType';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Display All Items</h1>
+      <div>
+        <HOF />
+
+      </div>
+      <h1>Display Based On User Type(Designer)</h1>
+      <div>
+        <UserType />
+
+      </div>
+      <h1>Filter All data starting with J</h1>
+
+      <div>
+        <FilterByLetter />
+
+      </div>
+      <h1>Filter All data based on age greater than 28 and age less than or equal to 50</h1>
+
+      <div>
+        <FilterByAge />
+
+      </div>
+      <h1>Filter total years of designers</h1>
+      <div>
+        <TotalExperience />
+      </div>
     </div>
   );
 }
